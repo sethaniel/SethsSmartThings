547122783cd3dfa7ecd685dd7f3f0a23848db36e
Seth Munroe <github.seth@spamcatcher.org>
2016-12-24 19:05:34 -0800

    test update

--- smartapps/sethaniel/lock-switcher.src/lock-switcher.groovy
+++ smartapps/sethaniel/lock-switcher.src/lock-switcher.groovy
@@ -1,7 +1,7 @@
 /**
  *  Lock Switcher
  *
- *  Copyright 2016 Seth Munroe
+ *  Copyright 2016 Seth Munroe (456)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
